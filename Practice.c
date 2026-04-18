85894cc8c98f000a2d0dfd076950967287380c37
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-18 22:39:16 +0600

    Update Practice.c

--- Practice.c
+++ Practice.c
@@ -56,3 +56,33 @@ printf("%d %d",mn,position);
 return 0;
 }
 
+
+
+
+
+
+#include<stdio.h>
+
+int main(){
+
+//A. Summation
+
+    int n;
+    scanf("%d",&n);
+long long int array[n];
+    for(int i = 0; i<n;i++){
+        scanf("%lld",&array[i]);
+    }
+long long int sum=0;
+for(int i = 0;i<n;i++){
+    sum = sum + array[i];
+}
+if(sum<0){
+    sum = sum*-1;
+}
+
+printf("%lld",sum);
+
+
+return 0;
+}
