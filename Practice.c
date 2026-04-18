a4452a03234b2cfab3c0155e877cc4b02e2afc4c
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-18 22:53:45 +0600

    Update Practice.c

--- Practice.c
+++ Practice.c
@@ -86,3 +86,34 @@ printf("%lld",sum);
 
 return 0;
 }
+
+
+
+
+
+#include<stdio.h>
+int main(){
+
+//B. Searching
+
+    int n;
+    scanf("%d",&n);
+int array[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&array[i]);
+    }
+int x,index=-1;
+scanf("%d",&x);
+
+for(int i = 0;i<n;i++){
+    if(array[i]==x){
+index = i;
+break;
+    }
+   
+
+}
+
+printf("%d",index);
+return 0;
+}
