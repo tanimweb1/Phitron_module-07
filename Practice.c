126f1c5837d9890b71335cf0ae10ca1fa92db07d
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-18 23:16:48 +0600

    Update Practice.c

--- Practice.c
+++ Practice.c
@@ -155,3 +155,32 @@ for(int i=0;i<n;i++){
 
 return 0;
 }
+
+
+
+
+
+
+#include<stdio.h>
+int main(){
+
+//D. Positions in array
+
+    int n;
+    scanf("%d",&n);
+int array[n];
+for(int i=0;i<n;i++){
+    scanf("%d",&array[i]);
+}
+int index;
+for(int i = 0;i<n;i++){
+
+if(array[i]<=10){
+  printf("A[%d] = %d\n",i,array[i]);
+}
+
+}
+
+
+return 0;
+}
